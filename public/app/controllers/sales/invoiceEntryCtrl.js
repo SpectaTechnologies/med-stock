33a62ca389ee532ef6c89eacdc89f5f8d1f78868
Vishal Ranjan <vishivish18@gmail.com>
2016-01-14 22:55:03 +0530

    typeahead working for company and products

--- public/app/controllers/sales/invoiceEntryCtrl.js
+++ public/app/controllers/sales/invoiceEntryCtrl.js
@@ -1,102 +1,135 @@
 angular.module('app')
     .controller('invoiceEntryCtrl', function($scope, $http) {
+        $scope.model = {};
+
+        $scope.availableProducts = function() {
+            $http.get('/api/products')
+                .then(function(response) {
+                    $scope.model.products = _.map(response.data, function(obj) {
+                        return {
+                            name: obj.product_name,
+                            code: obj.product_code
+                        }
+
+                    })
+
+                }, function(err) {
+                    console.log(err)
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                });
+        }
 
 
-        $scope.today = function() {
-            $scope.dt = new Date();
-        };
-        $scope.today();
+        $scope.availableCompanies = function() {
+            $http.get('/api/companies')
+                .then(function(response) {
+                    $scope.model.companies = _.map(response.data, function(obj) {
+                        return {
+                            name: obj.company_name,
+                            address: obj.company_address
+                        }
 
-        $scope.clear = function() {
-            $scope.dt = null;
-        };
+                    })
+                    console.log($scope.model.companies);
 
-        // Disable weekend selection
-        $scope.disabled = function(date, mode) {
-            return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-        };
+                }, function(err) {
+                    console.log(err)
+                        // called asynchronously if an error occurs
+                        // or server returns response with an error status.
+                });
+        }
 
-        $scope.toggleMin = function() {
-            $scope.minDate = $scope.minDate ? null : new Date();
-        };
 
-        $scope.toggleMin();
-        $scope.maxDate = new Date(2020, 5, 22);
+        $scope.setup = function() {
+            $scope.availableProducts();
+            $scope.availableCompanies();
+        }
 
-        $scope.open1 = function() {
-            $scope.popup1.opened = true;
-        };
+        $scope.setup();
 
-        $scope.open2 = function() {
-            $scope.popup2.opened = true;
-        };
 
-        $scope.setDate = function(year, month, day) {
-            $scope.dt = new Date(year, month, day);
-        };
 
-        $scope.dateOptions = {
-            formatYear: 'yy',
-            startingDay: 1,
-            showWeeks: false
-        };
 
-        $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-        $scope.format = $scope.formats[0];
-        $scope.altInputFormats = ['M!/d!/yyyy'];
+        function calendarImplementation() {
+            $scope.today = function() {
+                $scope.dt = new Date();
+            };
+            $scope.today();
 
-        $scope.popup1 = {
-            opened: false
-        };
+            $scope.clear = function() {
+                $scope.dt = null;
+            };
 
-        $scope.popup2 = {
-            opened: false
-        };
+            /* // Disable weekend selection
+             $scope.disabled = function(date, mode) {
+                 return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+             };*/
 
-        var tomorrow = new Date();
-        tomorrow.setDate(tomorrow.getDate() + 1);
-        var afterTomorrow = new Date();
-        afterTomorrow.setDate(tomorrow.getDate() + 1);
-        $scope.events = [{
-            date: tomorrow,
-            status: 'full'
-        }, {
-            date: afterTomorrow,
-            status: 'partially'
-        }];
+            $scope.toggleMin = function() {
+                $scope.minDate = $scope.minDate ? null : new Date();
+            };
 
-        $scope.getDayClass = function(date, mode) {
-            if (mode === 'day') {
-                var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+            $scope.toggleMin();
+            $scope.maxDate = new Date(2020, 5, 22);
 
-                for (var i = 0; i < $scope.events.length; i++) {
-                    var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+            $scope.open1 = function() {
+                $scope.popup1.opened = true;
+            };
 
-                    if (dayToCheck === currentDay) {
-                        return $scope.events[i].status;
+            $scope.open2 = function() {
+                $scope.popup2.opened = true;
+            };
+
+            $scope.setDate = function(year, month, day) {
+                $scope.dt = new Date(year, month, day);
+            };
+
+            $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1,
+                showWeeks: false
+            };
+
+            $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+            $scope.format = $scope.formats[0];
+            $scope.altInputFormats = ['M!/d!/yyyy'];
+
+            $scope.popup1 = {
+                opened: false
+            };
+
+            $scope.popup2 = {
+                opened: false
+            };
+
+            var tomorrow = new Date();
+            tomorrow.setDate(tomorrow.getDate() + 1);
+            var afterTomorrow = new Date();
+            afterTomorrow.setDate(tomorrow.getDate() + 1);
+            $scope.events = [{
+                date: tomorrow,
+                status: 'full'
+            }, {
+                date: afterTomorrow,
+                status: 'partially'
+            }];
+
+            $scope.getDayClass = function(date, mode) {
+                if (mode === 'day') {
+                    var dayToCheck = new Date(date).setHours(0, 0, 0, 0);
+
+                    for (var i = 0; i < $scope.events.length; i++) {
+                        var currentDay = new Date($scope.events[i].date).setHours(0, 0, 0, 0);
+
+                        if (dayToCheck === currentDay) {
+                            return $scope.events[i].status;
+                        }
                     }
                 }
-            }
 
-            return '';
-        };
-        //$scope.states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Dakota', 'North Carolina', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
-
-        console.log("in invoice ctrl")
-        $scope.availableProducts = function() {
-            console.log("in invoice ctrl function")
-            $http.get('/api/products')
-                .then(function(response) {                    
-                    $scope.model = _.map(response.data, function(obj) {
-                        return obj.product_name;
-                      
-                    })
-                    console.log($scope.model);
-
-                }, function(response) {
-                    // called asynchronously if an error occurs
-                    // or server returns response with an error status.
-                });
+                return '';
+            };
         }
 
 
